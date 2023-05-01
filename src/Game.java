e1ec1117a7ce6de5b70e3b330045797d92cb53e4
HARMONSC21 <harmonsc21@gcc.edu>
2023-04-30 20:39:09 -0400

    created first version of Game constructor and switchPlayer method

--- src/Game.java
+++ src/Game.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Game {
     private Board gameBoard;
 
@@ -11,7 +13,13 @@ public class Game {
      * the game constructor
      */
     public Game() {
-
+        Scanner in = new Scanner(System.in);
+        System.out.print("Player 1 enter your name: ");
+        player1 = in.next();
+        System.out.print("Player 2 enter your name: ");
+        player2 = in.next();
+        gameBoard = new Board(player1, player2);
+        currentPlayer = player1;
     }
 
     /**
@@ -25,6 +33,10 @@ public class Game {
      * switch the current player
      */
     private void switchPlayer() {
-
+        if (currentPlayer.equals(player1)) {
+            currentPlayer = player2;
+        } else {
+            currentPlayer = player1;
+        }
     }
 }
